7fb630bb5d3136f4c6106f236daebddb9cf1981a
Bysl-soft-09 <debaray14@gmail.com>
2021-07-06 12:23:55 +0600

    Implement findOne  method

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -51,8 +51,19 @@ exports.findAll = (req,res) => {
 
 //Find a single Tutorial with an id
 exports.findOne = (req,res) => {
+    const id = req.parms.id;
 
-}
+    Tutorial.findByPk(id)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Error retriving Tutorial with id="+id
+        });
+    });
+
+};
 
 //Update a Tutorial by the id in the request
 exports.delete = (req,res) => {

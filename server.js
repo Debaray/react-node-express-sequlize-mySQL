0985d5cf812a4498bdcf96792bec43793501597b
Bysl-soft-09 <debaray14@gmail.com>
2021-07-06 11:01:49 +0600

    add Server code

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = requir("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+//parse requestes of content type- application/json
+app.use(bodyParser.json());
+
+//parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+//simple route
+app.get("/", (req,res) =>{
+    res.json({message: "Welcome to the application."});
+});
+
+//set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>{
+
+    console.log(`Server is running on port ${PORT}`);
+
+})
